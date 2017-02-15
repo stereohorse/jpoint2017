a1d7a0b2ed6c9676d4e835172b122d5afaabeb48
Nguyen Viet <tnguen@alfabank.ru>
2017-02-15 16:39:32 +0300

    fix: set proper mems

--- reports/demo-5/report-holywars.sh
+++ reports/demo-5/report-holywars.sh
@@ -9,13 +9,13 @@ sleep 1
 # -- stress test
 
 # initiate app update
-(cd ../../vms && ansible-playbook -i vagrant play-update-names-api-mesos.yml && echo "==> app updated") &
+(cd ../../vms && ansible-playbook -i vagrant play-app-mesos.yml -t names && echo "==> app updated") &
 
 # simulate load
 REPORT_NAME=holywars-api
 
 echo "==> simulating load"
 
-echo "GET http://192.168.5.3/holywars-api/holywar" | vegeta attack -duration=15s -rate=150 > $REPORT_NAME.bin
+echo "GET http://192.168.5.3/holywars-api/holywar" | vegeta attack -duration=40s -rate=150 > $REPORT_NAME.bin
 cat $REPORT_NAME.bin | vegeta report
 cat $REPORT_NAME.bin | vegeta report -reporter=plot > $REPORT_NAME.html
